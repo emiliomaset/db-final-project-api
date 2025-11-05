9bc778d66de877be973f4e7fd460031a2dcbbf58
Attman585 <attman585@icloud.com>
2025-11-04 23:57:29 -0500

    Created basis for Search endpoint

--- src/main/java/com/example/streaming_service/controller/SearchController.java
+++ src/main/java/com/example/streaming_service/controller/SearchController.java
@@ -1,6 +1,29 @@
 package com.example.streaming_service.controller;
 
+import com.example.streaming_service.model.Content;
+import com.example.streaming_service.model.Episode;
+import com.example.streaming_service.repository.ContentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+@Service
+@RestController
 public class SearchController
 {
-
+    @GetMapping("/search")
+    public String search(@RequestParam(name = "q", required = false) String query)
+    {
+        if (query == null || query.isBlank())
+        {
+            return "Please type SOMETHING in...";
+            //return new RedirectView("https://www.google.com/");
+        }
+        return ContentRepository.findByTitleContaining(query.trim());          // Echo back exactly what was asked
+        //return new RedirectView(target, true);
+    }
 }
