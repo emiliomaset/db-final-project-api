2f67d184f2982036ac78d282af9946cb71415937
Attman585 <attman585@icloud.com>
2025-11-13 08:00:29 -0500

    Updated SearchController

--- src/main/java/com/example/streaming_service/controller/SearchController.java
+++ src/main/java/com/example/streaming_service/controller/SearchController.java
@@ -3,28 +3,35 @@ package com.example.streaming_service.controller;
 import com.example.streaming_service.model.Content;
 import com.example.streaming_service.model.Episode;
 import com.example.streaming_service.repository.ContentRepository;
+import com.example.streaming_service.service.LoginService;
+import com.example.streaming_service.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Service
 @RestController
 public class SearchController
 {
+    @Autowired
+    private SearchService searchService;
 
-//    @GetMapping("/search")
-//    public String search(@RequestParam(name = "q", required = false) String query)
-//    {
-//        if (query == null || query.isBlank())
-//        {
-//            return "Please type SOMETHING in...";
-//            //return new RedirectView("https://www.google.com/");
-//        }
-//        return ContentRepository.findByTitleContaining(query.trim());          // Echo back exactly what was asked
-//        //return new RedirectView(target, true);
-//    }
+    @GetMapping("/search")
+    public List search(@RequestParam(name = "query", required = false) String query,
+                       @RequestParam(name = "award", required = false) boolean awardCheck,
+                       @RequestParam(name = "watched", required = false) boolean watchedCheck)
+    {
+        if (query == null || query.isBlank())
+        {
+            return new ArrayList<String>();
+        }
+
+        return searchService.search(query.trim(), awardCheck, watchedCheck);          // Echo back exactly what was asked
+    }
+
 }
