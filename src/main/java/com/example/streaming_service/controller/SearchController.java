baf109c3773a5ee57f44cb5dbf57c37055c88a26
Mohamed-Elhelaly <melhelalyy@gmail.com>
2025-12-02 18:24:24 -0500

    Fix merge conflicts and restore correct ContentController implementation

--- src/main/java/com/example/streaming_service/controller/SearchController.java
+++ src/main/java/com/example/streaming_service/controller/SearchController.java
@@ -8,6 +8,7 @@ import com.example.streaming_service.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class SearchController
 {
     @Autowired
--- src/main/java/com/example/streaming_service/model/User.java
+++ src/main/java/com/example/streaming_service/model/User.java
@@ -3,117 +3,50 @@ package com.example.streaming_service.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "`user`")
 public class User
 {
     @Id
     @GeneratedValue
-    private String userId; //Primary key
+    @Column(name = "user_id")
+    private String userId;
 
     private String email;
     private String name;
+
+    @Column(name = "street_name")
     private String streetName;
+
     private String city;
     private String state;
     private String zip;
+
+    @Column(name = "phone_num")
     private String phoneNum;
+
     private String password;
-    private boolean isAdmin;
+
     public User() {}
 
-    //Setters
-    public void setUserId(String userId)
-    {
-        this.userId = userId;
-    }
+    // Setters
+    public void setUserId(String userId) { this.userId = userId; }
+    public void setEmail(String email) { this.email = email; }
+    public void setName(String name) { this.name = name; }
+    public void setStreetName(String streetName) { this.streetName = streetName; }
+    public void setCity(String city) { this.city = city; }
+    public void setState(String state) { this.state = state; }
+    public void setZip(String zip) { this.zip = zip; }
+    public void setPhoneNum(String phoneNum) { this.phoneNum = phoneNum; }
+    public void setPassword(String password) { this.password = password; }
 
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public void setStreetName(String streetName)
-    {
-        this.streetName = streetName;
-    }
-
-    public void setCity(String city)
-    {
-        this.city = city;
-    }
-
-    public void setState(String state)
-    {
-        this.state = state;
-    }
-
-    public void setZip(String zip)
-    {
-        this.zip = zip;
-    }
-
-    public void setPhoneNum(String phoneNum)
-    {
-        this.phoneNum = phoneNum;
-    }
-
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
-public void setAdmin(boolean admin) {
-        this.isAdmin = admin;
-}
-    //Getters
-    public String getUserId()
-    {
-        return userId;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public String getCity()
-    {
-        return city;
-    }
-
-    public String getStreetName()
-    {
-        return streetName;
-    }
-
-    public String getState()
-    {
-        return state;
-    }
-
-    public String getZip()
-    {
-        return zip;
-    }
-
-    public String getPhoneNum()
-    {
-        return phoneNum;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-    public boolean isAdmin(){
-        return isAdmin;
-    }
+    // Getters
+    public String getUserId() { return userId; }
+    public String getEmail() { return email; }
+    public String getName() { return name; }
+    public String getStreetName() { return streetName; }
+    public String getCity() { return city; }
+    public String getState() { return state; }
+    public String getZip() { return zip; }
+    public String getPhoneNum() { return phoneNum; }
+    public String getPassword() { return password; }
 }
